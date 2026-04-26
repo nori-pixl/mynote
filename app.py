07e0bda971ee71c0c65634cbead598c33d5ff57a
nori-pixl <sato.shigure4@gmail.com>
2026-04-27 01:04:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -161,5 +161,7 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    # 自分のPCで動かすのでポート5000、全ホスト許可
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    # Renderの環境変数からポートを取得、なければ5000
+    port = int(os.environ.get("PORT", 5000))
+    # Renderでは eventlet や gevent などのサーバーエンジンが必要
+    socketio.run(app, host='0.0.0.0', port=port)
